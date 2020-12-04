2bdc8f8942670b95fccc9e59909c7811d65eecd2
Scott Mangiapane <scottmangiapane@gmail.com>
2020-12-03 23:00:59 -0700

    Day 4 cleanup

--- day04/index.js
+++ day04/index.js
@@ -5,31 +5,64 @@ const input = fs
     .readFileSync('day04/input.txt')
     .toString().trim().split('\n\n');
 
+function getMap(passport) {
+    const attributes = passport.replace(/\n/g, ' ').split(' ');
+    const map = {};
+    attributes.forEach(attribute => {
+        const [key, val] = attribute.split(':');
+        map[key] = val;
+    });
+    return map;
+}
+
+function hasRequiredAttributes(map) {
+    return 'byr' in map
+        && 'iyr' in map
+        && 'eyr' in map
+        && 'hgt' in map
+        && 'hcl' in map
+        && 'ecl' in map
+        && 'pid' in map;
+}
+
+function hasValidYears(map) {
+    return map.byr >= 1920 && map.byr <= 2002
+        && map.iyr >= 2010 && map.iyr <= 2020
+        && map.eyr >= 2020 && map.eyr <= 2030;
+}
+
+function hasValidHeight(map) {
+    try {
+        const [, hgt] = /^(.+)(cm|in)$/.exec(map.hgt);
+        return (map.hgt.includes('in') && hgt >= 59 && hgt <= 76)
+            || (map.hgt.includes('cm') && hgt >= 150 && hgt <= 193);
+    } catch (err) { /* ignored */ }
+    return false;
+}
+
+function hasValidHairColor(map) {
+    return /^#[0-9a-f]{6}$/.test(map.hcl);
+}
+
+function hasValidEyeColor(map) {
+    return /^amb|blu|brn|gry|grn|hzl|oth$/.test(map.ecl);
+}
+
+function hasValidPassportId(map) {
+    return /^[0-9]{9}$/.test(map.pid);
+}
+
 let count1 = 0, count2 = 0;
-for (const line of input) {
-    const keys = line.replace(/\n/g, ' ').split(' ');
-    if (line.includes('byr:') && line.includes('iyr:') && line.includes('eyr:') && line.includes('hgt:') && line.includes('hcl:') && line.includes('ecl:') && line.includes('pid:')) {
+for (const passport of input) {
+    const map = getMap(passport);
+    if (hasRequiredAttributes(map)) {
         count1++;
-        let map = {};
-        for (let i = 0; i < keys.length; i++) {
-            const keyval = keys[i].split(':');
-            map[keyval[0]] = keyval[1];
-        }
-        if (map.byr >= 1920 && map.byr <= 2002 && map.iyr >= 2010 && map.iyr <= 2020 && map.eyr >= 2020 && map.eyr <= 2030) {
-            console.log('1: ' + map.hgt);
-            if (/^(.+)(cm|in)$/.test(map.hgt)) {
-                const [, hgt] = /^(.+)(cm|in)$/.exec(map.hgt);
-                console.log(hgt);
-                if ((map.hgt.includes('in') && hgt >= 59 && hgt <= 76) || (map.hgt.includes('cm') && hgt >= 150 && hgt <= 193)) {
-                    if (/^#[0-9a-f]{6}$/.test(map.hcl)) {
-                        if (/^amb|blu|brn|gry|grn|hzl|oth$/.test(map.ecl)) {
-                            if (/^[0-9]{9}$/.test(map.pid)) {
-                                count2++;
-                            }
-                        }
-                    }
-                }
-            }
+        if (hasValidYears(map)
+            && hasValidHeight(map)
+            && hasValidHairColor(map)
+            && hasValidEyeColor(map)
+            && hasValidPassportId(map)) {
+            count2++;
         }
     }
 }

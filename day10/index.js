d136e6e9a559a044102dbffb7d5e2656e11ced2f
Scott Mangiapane <scottmangiapane@gmail.com>
2020-12-03 19:07:23 -0700

    Added base templates

---
+++ day10/index.js
@@ -0,0 +1,11 @@
+const chalk = require('chalk');
+const fs = require('fs');
+
+const input = fs
+    .readFileSync('day10/input.txt')
+    .toString().trim().split('\n');
+
+for (const line of input) {}
+
+console.log('Part 1: ' + chalk.green());
+console.log('Part 2: ' + chalk.green());

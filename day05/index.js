44ee65ce6f7a6445416327776c9a67fb93036223
Scott Mangiapane <scottmangiapane@gmail.com>
2020-12-04 22:42:44 -0700

    Day 5 cleanup

--- day05/index.js
+++ day05/index.js
@@ -1,70 +1,37 @@
 const chalk = require('chalk');
-const { assert } = require('console');
 const fs = require('fs');
-const { listeners } = require('process');
 
 const input = fs
     .readFileSync('day05/input.txt')
     .toString().trim().split('\n');
 
-let startRow;
-let endRow;
-
-let startCol;
-let endCol;
-
-function diveFB(char) {
-    const half = (endRow - startRow + 1) / 2;
-    if (char === 'B') {
-        startRow += half;
+function search(input, low, high, lowChar, highChar) {
+    if (low === high) {
+        return low;
     }
-    if (char === 'F') {
-        endRow -= half;
+    const half = (high - low + 1) / 2;
+    if (input.charAt(0) === lowChar) {
+        return search(input.substring(1), low, high - half, lowChar, highChar);
+    }
+    if (input.charAt(0) === highChar) {
+        return search(input.substring(1), low + half, high, lowChar, highChar);
     }
 }
 
-function diveLR(char) {
-    const half = (endCol - startCol + 1) / 2;
-    if (char === 'R') {
-        startCol += half;
-    }
-    if (char === 'L') {
-        endCol -= half;
-    }
-}
-
-let part1 = 0;
-let part2 = 0;
 let seats = [];
 
 for (const line of input) {
-    startRow = 0;
-    endRow = 127;
-    for (let i = 0; i < 7; i++) {
-        diveFB(line.charAt(i));
-    }
-    if (startRow != endRow) {
-        console.log('ERROR row');
-    }
-    // console.log('row: ' + startRow);
-
-    startCol = 0;
-    endCol = 7;
-    for (let i = 7; i < 10; i++) {
-        diveLR(line.charAt(i));
-    }
-    if (startCol != endCol) {
-        console.log('ERROR col');
-    }
-    // console.log('col: ' + startCol);
-
-    const score = startRow * 8 + startCol;
-    part1 = Math.max(part1, score);
-
-    seats.push(score);
+    const [, rowLine, colLine] = /^([BF]+)([LR]+)$/.exec(line);
+    const row = search(rowLine, 0, 127, 'F', 'B');
+    const col = search(colLine, 0, 7, 'L', 'R');
+    seats.push(row * 8 + col);
 }
 
 seats = seats.sort((a, b) => parseInt(a) - parseInt(b));
+
+const part1 = seats[seats.length - 1];
+
+let part2;
 for (let i = 0; i < seats.length - 1; i++) {
     if (seats[i + 1] != seats[i] + 1) {
         part2 = seats[i] + 1;
@@ -73,5 +40,3 @@ for (let i = 0; i < seats.length - 1; i++) {
 
 console.log('Part 1: ' + chalk.green(part1));
 console.log('Part 2: ' + chalk.green(part2));
-
-// console.log(JSON.stringify(seats));

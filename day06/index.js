66b89e4da6b3d9938bf4579efc362dd6965c1012
Scott Mangiapane <scottmangiapane@gmail.com>
2020-12-05 22:22:50 -0700

    Day 6 quick solution

--- day06/index.js
+++ day06/index.js
@@ -3,9 +3,37 @@ const fs = require('fs');
 
 const input = fs
     .readFileSync('day06/input.txt')
-    .toString().trim().split('\n');
+    .toString().trim().split('\n\n');
 
-for (const line of input) {}
+let sum1 = 0;
+for (const line of input) {
+    const parsed = line.replace(/\n/g, '');
+    const questions = {};
+    for (let i = 0; i < parsed.length; i++) {
+        questions[parsed[i]] = true;
+    }
+    sum1 += Object.keys(questions).length;
+}
 
-console.log('Part 1: ' + chalk.green());
-console.log('Part 2: ' + chalk.green());
+let sum2 = 0;
+for (const line of input) {
+    const parsed = line.replace(/\n/g, '');
+    const questions = {};
+    for (let i = 0; i < parsed.length; i++) {
+        if (!!questions[parsed[i]]) {
+            questions[parsed[i]]++;
+        } else {
+            questions[parsed[i]] = 1;
+        }
+    }
+    console.log(line.length + '; ' + JSON.stringify(questions));
+    for (const i in questions) {
+        if (questions[i] != line.split(/\r\n|\r|\n/).length) {
+            delete questions[i];
+        }
+    }
+    sum2 += Object.keys(questions).length;
+}
+
+console.log('Part 1: ' + chalk.green(sum1));
+console.log('Part 2: ' + chalk.green(sum2));

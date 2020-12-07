d2910d3166b4b7c3695b2255bb5fbf183d593672
Scott Mangiapane <scottmangiapane@gmail.com>
2020-12-06 23:01:23 -0700

    Day 7 quick solution

--- day01/index.js
+++ day01/index.js
@@ -14,13 +14,6 @@ for (let i = 0; i < input.length; i++) {
             console.log('num2: ' + num2);
             console.log('Part 1: ' + chalk.green(num1 * num2));
         }
-    }
-}
-
-for (let i = 0; i < input.length; i++) {
-    const num1 = parseInt(input[i]);
-    for (let j = i + 1; j < input.length; j++) {
-        const num2 = parseInt(input[j]);
         for (let k = j + 1; k < input.length; k++) {
             const num3 = parseInt(input[k]);
             if (num1 + num2 + num3 === 2020) {
--- day07/index.js
+++ day07/index.js
@@ -5,7 +5,51 @@ const input = fs
     .readFileSync('day07/input.txt')
     .toString().trim().split('\n');
 
-for (const line of input) {}
+let colors = [ 'shiny gold' ];
 
-console.log('Part 1: ' + chalk.green());
-console.log('Part 2: ' + chalk.green());
+for (let i = 0; i < 100; i++) { // arbitrarily high number
+    for (const line of input) {
+        const [, parent, remainder] = /^(.+) bags contain (.+)\.$/.exec(line);
+        if (remainder !== 'no other bags') {
+            const children = remainder.split(', ');
+            for (const child of children) {
+                const [, number, color] = /^([0-9]+) (.+) bags?$/.exec(child);
+
+                if (!colors.includes(parent) && colors.includes(color)) {
+                    colors.push(parent);
+                }
+            }
+        }
+    }
+}
+
+const part1 = colors.length - 1;
+
+function searchInput(color) {
+    let count = 1;
+
+    for (const line of input) {
+        const [, parent, remainder] = /^(.+) bags contain (.+)\.$/.exec(line);
+        if (parent === color) {
+            console.log('looking for ' + color + ', child: ' + remainder);
+            // console.log(count + ' so far');
+            if (remainder === 'no other bags') {
+                return 1;
+            }
+            const children = remainder.split(', ');
+            for (const child of children) {
+                console.log(child);
+                const [, childNumber, childColor] = /^([0-9]+) (.+) bags?$/.exec(child);
+
+                count += childNumber * searchInput(childColor);
+            }
+        }
+    }
+
+    return count;
+}
+
+const part2 = searchInput('shiny gold') - 1;
+
+console.log('Part 1: ' + chalk.green(part1));
+console.log('Part 2: ' + chalk.green(part2));

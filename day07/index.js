57f2dc7bfb586d30f4228a8d8fd5f25eeb48b07a
Scott Mangiapane <scottmangiapane@gmail.com>
2020-12-06 23:57:34 -0700

    Day 7 cleanup

--- day07/index.js
+++ day07/index.js
@@ -5,51 +5,46 @@ const input = fs
     .readFileSync('day07/input.txt')
     .toString().trim().split('\n');
 
-let colors = [ 'shiny gold' ];
+let rules = [];
+let reverseRules = [];
 
-for (let i = 0; i < 100; i++) { // arbitrarily high number
-    for (const line of input) {
-        const [, parent, remainder] = /^(.+) bags contain (.+)\.$/.exec(line);
-        if (remainder !== 'no other bags') {
-            const children = remainder.split(', ');
-            for (const child of children) {
-                const [, number, color] = /^([0-9]+) (.+) bags?$/.exec(child);
-
-                if (!colors.includes(parent) && colors.includes(color)) {
-                    colors.push(parent);
-                }
-            }
+for (const line of input) {
+    const [, parent, remainder] = /^(.+) bags contain (.+)\.$/.exec(line);
+    rules[parent] = [];
+    if (remainder !== 'no other bags') {
+        const children = remainder.split(', ');
+        for (const child of children) {
+            const [, number, color] = /^([0-9]+) (.+) bags?$/.exec(child);
+            rules[parent].push({ number, color });
+            reverseRules[color] = reverseRules[color] || [];
+            reverseRules[color].push(parent);
         }
     }
 }
 
-const part1 = colors.length - 1;
-
-function searchInput(color) {
-    let count = 1;
-
-    for (const line of input) {
-        const [, parent, remainder] = /^(.+) bags contain (.+)\.$/.exec(line);
-        if (parent === color) {
-            console.log('looking for ' + color + ', child: ' + remainder);
-            // console.log(count + ' so far');
-            if (remainder === 'no other bags') {
-                return 1;
-            }
-            const children = remainder.split(', ');
-            for (const child of children) {
-                console.log(child);
-                const [, childNumber, childColor] = /^([0-9]+) (.+) bags?$/.exec(child);
-
-                count += childNumber * searchInput(childColor);
+function countParents(parentsFound, color) {
+    if (!parentsFound.includes(color)) {
+        parentsFound.push(color);
+        if (reverseRules[color]) {
+            for (const parent of reverseRules[color]) {
+                countParents(parentsFound, parent);
             }
         }
     }
+    return parentsFound.length;
+}
 
+const part1 = countParents([], 'shiny gold') - 1;
+
+function countChildren(color) {
+    let count = 1;
+    for (const child of rules[color]) {
+        count += child.number * countChildren(child.color);
+    }
     return count;
 }
 
-const part2 = searchInput('shiny gold') - 1;
+const part2 = countChildren('shiny gold') - 1;
 
 console.log('Part 1: ' + chalk.green(part1));
 console.log('Part 2: ' + chalk.green(part2));

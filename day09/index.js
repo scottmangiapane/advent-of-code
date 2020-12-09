e94331fbf984d5d86c2c497494632c77fa07b71f
Scott Mangiapane <scottmangiapane@gmail.com>
2020-12-08 22:53:43 -0700

    Day 9 quick solution

--- day09/index.js
+++ day09/index.js
@@ -3,9 +3,67 @@ const fs = require('fs');
 
 const input = fs
     .readFileSync('day09/input.txt')
-    .toString().trim().split('\n');
+    .toString().trim().split('\n')
+    .map(line => parseInt(line));
 
-for (const line of input) {}
+// const preamble = input;
 
-console.log('Part 1: ' + chalk.green());
+// const remainder = preamble.splice(25);
+
+// function isMatched(preamble, line) {
+//     for (let i = 0; i < preamble.length; i++) {
+//         for (let j = i + 1; j < preamble.length; j++) {
+//             if (preamble[i] + preamble[j] === line) {
+//                 return true;
+//             }
+//         }
+//     }
+//     return false;
+// }
+
+// let part1;
+
+// while (remainder.length > 0) {
+//     if (!isMatched(preamble, remainder[0])) {
+//         part1 = remainder[0];
+//         console.log('#!#! FOUND IT: ' + part1);
+//         break;
+//     }
+//     preamble.push(remainder.shift());
+//     preamble.shift();
+// }
+
+// console.log('Part 1: ' + chalk.green());
+
+let part1 = 144381670
+
+function getSum(array) {
+    let total = 0;
+    for (const i of array) {
+        total += i;
+    }
+    return total;
+}
+
+let pieces = [];
+
+while (true) {
+    while (getSum(pieces) < part1) {
+        pieces.push(input.shift());
+    }
+
+    if (getSum(pieces) === part1) {
+        pieces = pieces.sort((a,b) => a < b);
+        console.log('#!#! FOUND IT: ' + JSON.stringify(pieces));
+        console.log('#!#! sum: ' + getSum(pieces));
+        console.log('#!#! answer: ' + (pieces[0] + pieces[pieces.length - 1]));
+        process.exit();
+    }
+
+    pieces.shift();
+}
+
+// not 20285626
+// not 16250392
+
 console.log('Part 2: ' + chalk.green());

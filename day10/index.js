5623b8f9999d3a168d3d1c9398c69c2118fc996b
Scott Mangiapane <scottmangiapane@gmail.com>
2020-12-10 00:54:01 -0700

    Day 10 cleanup

--- day10/index.js
+++ day10/index.js
@@ -6,8 +6,9 @@ const input = fs
     .toString().trim().split('\n')
     .map(i => parseInt(i));
 
-input.push(0);
-input.push(Math.max(...input) + 3);
+const deviceAdapter = Math.max(...input) + 3;
+
+input.push(0, deviceAdapter);
 input.sort((a, b) => a - b);
 
 let j1 = 0;
@@ -23,14 +24,14 @@ for (let i = 0; i < input.length - 1; i++) {
     }
 }
 
-console.log('Part 1: ' + chalk.green(j1 * j3));
+console.log('j1: ' + j1);
+console.log('j3: ' + j3);
 
-function findIndexFromValue(val) {
-    return input.findIndex(v => v === val);
-}
+const part1 = j1 * j3;
+console.log('Part 1: ' + chalk.green(part1));
 
 function findParents(child) {
-    const index = findIndexFromValue(child);
+    const index = input.findIndex(val => val === child);
     return input.filter(val => val < input[index] && val >= input[index] - 3);
 }
 
@@ -38,28 +39,10 @@ const stepsTaken = {};
 
 for (const val of input) {
     const steps = findParents(val)
-        .map(parent => stepsTaken[parent] || 1) // TODO remove "|| 0"?
+        .map(parent => stepsTaken[parent] || 1)
         .reduce((a, b) => a + b, 0);
     stepsTaken[val] = steps;
 }
 
-console.log(JSON.stringify(stepsTaken));
-
-// function fetchChildren(parent) {
-//     return map[parent];
-// }
-
-// function countChildren(root) {
-//     const children = fetchChildren(root);
-//     if (children.length === 0) {
-//         return 1;
-//     }
-//     let count = 0;
-//     for (const child of children) {
-//         count += countChildren(child);
-//     }
-//     return count;
-// }
-
-// const part2 = countChildren(input[0]);
-// console.log('Part 2: ' + chalk.green(part2));
+const part2 = stepsTaken[deviceAdapter];
+console.log('Part 2: ' + chalk.green(part2));

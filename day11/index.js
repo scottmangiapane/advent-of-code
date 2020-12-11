9d4228ecb93f4984c1d5300d22c39ae65c950dc6
Scott Mangiapane <scottmangiapane@gmail.com>
2020-12-11 01:43:43 -0700

    Day 11 cleanup

--- day11/index.js
+++ day11/index.js
@@ -1,42 +1,64 @@
 const chalk = require('chalk');
-const { count } = require('console');
 const fs = require('fs');
 
 const input = fs
     .readFileSync('day11/input.txt')
     .toString().trim().split('\n');
 
-function findSeatInDirection(input, x, y, xDelta, yDelta, recursive) {
-    const initial = input[y][x];
-    x += xDelta;
-    y += yDelta;
-    if (x < 0 || y < 0 || x >= input[0].length || y >= input.length) { return false; }
-    if (input[y][x] === '#') { return true; }
-    if (input[y][x] === 'L') { return false; }
-    if (recursive) {
-        return findSeatInDirection(input, x, y, xDelta, yDelta, recursive);
+const height = input.length;
+const width = input[0].length;
+
+function compareLayouts(layout1, layout2) {
+    for (let i = 0; i < height; i++) {
+        if (layout1[i] !== layout2[i]) {
+            return false;
+        }
     }
-    return false;
+    return true;
+}
+
+function countOccupiedSeats(layout) {
+    let count = 0;
+    for (const line of layout) {
+        for (const char of line) {
+            if (char === '#') {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
+function findSeat(input, x, y, dx, dy, recursive) {
+    x += dx;
+    y += dy;
+    if (x < 0 || y < 0 || x >= width || y >= height) {
+        return false;
+    }
+    if (input[y][x] === '.' && recursive) {
+        return findSeat(input, x, y, dx, dy, recursive);
+    }
+    return (input[y][x] === '#');
 }
 
 function countAdjacent(input, x, y, recursive) {
     let count = 0;
-    if (findSeatInDirection(input, x, y, -1, -1, recursive)) { count++; }
-    if (findSeatInDirection(input, x, y, -1, 0, recursive)) { count++; }
-    if (findSeatInDirection(input, x, y, -1, 1, recursive)) { count++; }
-    if (findSeatInDirection(input, x, y, 0, -1, recursive)) { count++; }
-    if (findSeatInDirection(input, x, y, 0, 1, recursive)) { count++; }
-    if (findSeatInDirection(input, x, y, 1, -1, recursive)) { count++; }
-    if (findSeatInDirection(input, x, y, 1, 0, recursive)) { count++; }
-    if (findSeatInDirection(input, x, y, 1, 1, recursive)) { count++; }
+    if (findSeat(input, x, y, -1, -1, recursive)) { count++; }
+    if (findSeat(input, x, y, -1, 0, recursive)) { count++; }
+    if (findSeat(input, x, y, -1, 1, recursive)) { count++; }
+    if (findSeat(input, x, y, 0, -1, recursive)) { count++; }
+    if (findSeat(input, x, y, 0, 1, recursive)) { count++; }
+    if (findSeat(input, x, y, 1, -1, recursive)) { count++; }
+    if (findSeat(input, x, y, 1, 0, recursive)) { count++; }
+    if (findSeat(input, x, y, 1, 1, recursive)) { count++; }
     return count;
 }
 
 function calculateRound(input, tolerance, recursive) {
     const next = [];
-    for (let y = 0; y < input.length; y++) {
+    for (let y = 0; y < height; y++) {
         let newLine = '';
-        for (let x = 0; x < input[y].length; x++) {
+        for (let x = 0; x < width; x++) {
             const seat = input[y][x];
             const count = countAdjacent(input, x, y, recursive);
             if (seat === 'L' && count === 0) {
@@ -52,32 +74,19 @@ function calculateRound(input, tolerance, recursive) {
     return next;
 }
 
-function countOccupiedSeats(input) {
-    let count = 0;
-    for (const line of input) {
-        for (const char of line) {
-            if (char === '#') {
-                count++;
-            }
+function runTest(threshold, recursive) {
+    let layout = input;
+    while (true) {
+        let nextLayout = calculateRound(layout, threshold, recursive);
+        if (compareLayouts(layout, nextLayout)) {
+            return countOccupiedSeats(layout);
         }
+        layout = nextLayout;
     }
-    return count;
 }
 
-let nextP1 = JSON.parse(JSON.stringify(input));
-
-for (let i = 0; i < 500; i++) {
-    nextP1 = calculateRound(nextP1, 4, false);
-}
-
-const part1 = countOccupiedSeats(nextP1);
+const part1 = runTest(4, false);
 console.log('Part 1: ' + chalk.green(part1));
 
-let nextP2 = JSON.parse(JSON.stringify(input));
-
-for (let i = 0; i < 500; i++) {
-    nextP2 = calculateRound(nextP2, 5, true);
-}
-
-const part2 = countOccupiedSeats(nextP2);
+const part2 = runTest(5, true);
 console.log('Part 2: ' + chalk.green(part2));

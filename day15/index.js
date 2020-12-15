fbe5f13461c4abe383bdbbc589aa7467a2129168
Scott Mangiapane <scottmangiapane@gmail.com>
2020-12-14 22:51:10 -0700

    Day 15 quick solution

--- day15/index.js
+++ day15/index.js
@@ -3,9 +3,30 @@ const fs = require('fs');
 
 const input = fs
     .readFileSync('day15/input.txt')
-    .toString().trim().split('\n');
+    .toString().trim().split(',')
+    .map(e => parseInt(e));
 
-for (const line of input) {}
+function next(memory, last) {
+    const array = memory[last];
+    if (array.length === 1) {
+        return 0;
+    }
+    while (array.length > 2) {
+        array.shift();
+    }
+    return array[array.length - 1] - array[array.length - 2];
+}
 
-console.log('Part 1: ' + chalk.green());
-console.log('Part 2: ' + chalk.green());
+function solve(iterations) {
+    const memory = {};
+    let last = null;
+    for (let i = 0; i < iterations; i++) {
+        last = (i < input.length) ? input[i] : next(memory, last);
+        memory[last] = memory[last] || [];
+        memory[last].push(i);
+    }
+    return last;
+}
+
+console.log('Part 1: ' + chalk.green(solve(2020)));
+console.log('Part 2: ' + chalk.green(solve(30000000)));

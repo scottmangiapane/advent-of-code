056c675920a78e598900c9d24152c41efbd2a405
Scott Mangiapane <scottmangiapane@gmail.com>
2021-11-30 22:28:31 -0700

    Cleaned up solution for 2021 day 1

--- 2021/day01/index.js
+++ 2021/day01/index.js
@@ -4,28 +4,26 @@ import { promises as fs } from 'fs';
 const input = (await fs.readFile('2021/day01/input.txt'))
     .toString().trim().split('\n');
 
-let count = 0;
-let last = input[0];
-for (let i = 0; i < input.length; i++) {
-    const value = parseInt(input[i]);
-    if (value > last) {
-        count++;
+function solve(windowSize) {
+    let sum = 0;
+    for (let i = 0; i < windowSize && i < input.length; i++) {
+        sum += parseInt(input[i]);
     }
-    last = value;
+    let count = 0;
+    let last = sum;
+    for (let i = 0; i < input.length - windowSize; i++) {
+        sum -= parseInt(input[i]);
+        sum += parseInt(input[i + windowSize]);
+        if (sum > last) {
+            count++;
+        }
+        last = sum;
+    }
+    return count;
 }
 
-console.log(count);
+const p1 = solve(1);
+const p2 = solve(3);
 
-let count2 = 0;
-let last2 = 0;
-for (let i = 0; i < input.length - 2; i++) {
-    const sum = parseInt(input[i])
-        + parseInt(input[i + 1])
-        + parseInt(input[i + 2]);
-    if (sum > last2) {
-        count2++;
-    }
-    last2 = sum;
-}
-
-console.log(count2 - 1);
+console.log('Part 1: ' + chalk.green(p1));
+console.log('Part 2: ' + chalk.green(p2));

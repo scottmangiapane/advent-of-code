021ba24e31919f29f5406f9412186dc1e8e62628
Scott Mangiapane <scottmangiapane@gmail.com>
2021-12-01 22:09:36 -0700

    Initial implementation for 2021 day 2

---
+++ 2021/day02/index.js
@@ -0,0 +1,61 @@
+import chalk from 'chalk';
+import { promises as fs } from 'fs';
+
+const input = (await fs.readFile('2021/day02/input.txt'))
+    .toString().trim().split('\n');
+
+// function solveP1() {
+//     let x = 0;
+//     let depth = 0;
+//     for (const line of input) {
+//         const [, direction, amount] = /^([a-z]+) ([0-9]+)$/.exec(line);
+//         console.log('direction: ' + direction);
+//         console.log('amount: ' + amount);
+//         switch (direction) {
+//             case 'forward':
+//                 x += parseInt(amount);
+//                 break;
+//             case 'up':
+//                 depth -= parseInt(amount);
+//                 break;
+//             case 'down':
+//                 depth += parseInt(amount);
+//                 break;
+//         }
+//     }
+//     console.log('x: ' + x);
+//     console.log('depth: ' + depth);
+//     return x * depth;
+// }
+
+function solveP2() {
+    let x = 0;
+    let depth = 0;
+    let aim = 0;
+    for (const line of input) {
+        const [, direction, amount] = /^([a-z]+) ([0-9]+)$/.exec(line);
+        console.log('direction: ' + direction);
+        console.log('amount: ' + amount);
+        switch (direction) {
+            case 'forward':
+                x += parseInt(amount);
+                depth += aim * amount;
+                break;
+            case 'up':
+                aim -= parseInt(amount);
+                break;
+            case 'down':
+                aim += parseInt(amount);
+                break;
+        }
+    }
+    console.log('x: ' + x);
+    console.log('depth: ' + depth);
+    return x * depth;
+}
+
+// const p1 = solveP1();
+const p2 = solveP2();
+
+// console.log('Part 1: ' + chalk.green(p1));
+console.log('Part 2: ' + chalk.green(p2));
